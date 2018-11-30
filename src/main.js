084cd06625cac76f3f5b662b1a8a91e8dd2fd8ab
smdqromeo <1359247894@qq.com>
2018-11-30 21:38:49 +0800

    引入了字体图标同时运用stylus写样式，通过用stylus写变量控制样式

--- src/main.js
+++ src/main.js
@@ -3,8 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+// 重置样式和border初识化样式
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
+import './assets/styles/iconfont.css'
+
 
 Vue.config.productionTip = false
+//移动端300hs问题
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ src/page/home/components/Header.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="header">
+    
+    <div class="header-left">
+      <span class="iconfont back-icon">&#xe624;</span>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+      输入城市/景点/游玩主题</div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arrow-icon">&#xe65e;</span>
+     </div>
+  </div>
+</template>
+<script>
+  export default {
+    name:'HomeHeader'
+  }
+  // @import '../../../assets/styles/varibles.styl'在build 文件里webpack.base 配置了变量  可改为
+  // @import '~styles/assets/styles/varibles.styl'
+</script>
+<style scoped lang="stylus">
+@import '~styles/varibles.styl'
+
+  .header 
+    display:flex
+    height:.86rem
+    background: $bgColor
+    color: #fff
+    .header-left
+      text-align:center
+      line-height:.86rem
+      width 0.64rem
+      float:left
+       .back-icon
+         font-size: .4rem
+    .header-right
+      width: 1.24rem
+      float:right
+      text-align:center
+      line-height: .86rem
+      .arrow-icon
+        font-size: .24rem
+    .header-input
+      flex:1
+      background:#fff
+      border-radius:.1rem
+      margin-top:.12rem
+      line-height: .64rem
+      height: .64rem
+      margin-left:.2rem
+      color:#ccc
+
+    
+</style>
+
