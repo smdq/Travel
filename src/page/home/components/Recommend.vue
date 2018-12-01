8d4e1cad5c93a5502612a0177afaeda8acc98552
smdqromeo <1359247894@qq.com>
2018-12-01 21:59:15 +0800

    add recommend/add weekend

---
+++ src/page/home/components/Recommend.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+        
+        <img class="item-img" :src="item.imgUrl" alt="">
+        
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+          <button class="item-button">查看详情</button>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+  export default {
+    name:'HomeRecommend',
+    data(){
+      return {
+        recommendList:[
+          {
+            id:'0001',
+            imgUrl:'http://img1.qunarzz.com/sight/p0/1508/12/4e6170303c63bdb7070fb1987480afab.water.jpg_200x200_0775a71c.jpg',
+            title:'杭州极地海洋世界',
+            desc:'世界三大名秀之一，杭州宋城的灵魂杭州宋城'
+          },
+          {
+            id:'0001',
+            imgUrl:'http://img1.qunarzz.com/sight/p0/1508/12/4e6170303c63bdb7070fb1987480afab.water.jpg_200x200_0775a71c.jpg',
+            title:'杭州极地海洋世界',
+            desc:'世界三大名秀之一，杭州宋城的灵魂杭州宋城'
+          },{
+            id:'0001',
+            imgUrl:'http://img1.qunarzz.com/sight/p0/1508/12/4e6170303c63bdb7070fb1987480afab.water.jpg_200x200_0775a71c.jpg',
+            title:'杭州极地海洋世界',
+            desc:'世界三大名秀之一，杭州宋城的灵魂杭州宋城'
+          }
+        ]
+      }
+    }
+  }
+  // 样式中的... 同时在item-info 中设置了min-width:0 才有效果
+</script >
+<style lang="stylus" scoped>
+ .title
+   margin-top: .2rem
+   line-height: .8rem
+   background: #eeeeee
+   text-indent: .2rem
+ .item
+   overflow:hidden
+   display:flex
+   height: 1.9rem
+   .item-img
+     width: 1.7rem
+     height 1.7rem
+     padding: .1rem
+   .item-info
+     flex: 1
+     padding: .1rem
+     min-width:0
+     .item-title
+       line-height: .54rem
+       font-size: .32rem
+     .item-desc
+       line-height: .4rem
+       color: #eee
+       overflow: hidden
+       white-space: nowrap 
+       text-overflow:ellipsis
+     .item-button
+       background #ff9300
+       padding: 0 .1rem
+       color: #ffffff
+       border-radius: .06rem
+     
+</style>
+
---
+++ src/page/home/components/Weekend.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <div class="title">周末去哪</div>
+    <ul>
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+       <div class="item-img-wrapper" >
+        <img class="item-img" :src="item.imgUrl" alt="">
+       </div> 
+        <div class="item-info">
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+  export default {
+    name:'HomeWeekend',
+    data(){
+      return {
+        recommendList:[
+          {
+            id:'0001',
+            imgUrl:'http://img1.qunarzz.com/sight/source/1509/fb/c45a951a9b4305.jpg_r_640x214_26fcfd26.jpg',
+            title:'温州动人秋色',
+            desc:'细数温州那些美到陶醉的赏秋地'
+          },
+           {
+            id:'0002',
+            imgUrl:'http://img1.qunarzz.com/sight/source/1509/fb/c45a951a9b4305.jpg_r_640x214_26fcfd26.jpg',
+            title:'温州动人秋色',
+            desc:'细数温州那些美到陶醉的赏秋地'
+          },
+           {
+            id:'0003',
+            imgUrl:'http://img1.qunarzz.com/sight/source/1509/fb/c45a951a9b4305.jpg_r_640x214_26fcfd26.jpg',
+            title:'温州动人秋色',
+            desc:'细数温州那些美到陶醉的赏秋地'
+          }
+         
+        ]
+      }
+    }
+  }
+  // 样式中的... 同时在item-info 中设置了min-width:0 才有效果
+</script >
+<style lang="stylus" scoped>
+.title
+   margin-top: .2rem
+   line-height: .8rem
+   background: #eeeeee
+   text-indent: .2rem
+ .item-img-wrapper
+   overflow:hidden
+   height: 0
+   padding-bottom: 33.9%
+    
+  .item-img
+    width: 100%
+.item-info
+  padding: .1rem
+  min-width:0
+  .item-title
+    line-height: .54rem
+    font-size: .32rem
+  .item-desc
+    line-height: .4rem
+    color: #eee
+    overflow: hidden
+    white-space: nowrap 
+    text-overflow:ellipsis
+ 
+     
+</style>
+
