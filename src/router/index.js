a3366ea9f4983aedb4b4aec38c27690f6ca0bcf9
smdqromeo <1359247894@qq.com>
2018-12-05 21:14:23 +0800

    项目联调时要通过设置config里面的index开发路径进行，当要进行测试时通过ipconfig找到本机号同时在package.json中加 --host 0.0.0.0 在相同的开发环境中进行真机联调、打包上线可以直接npm  run  build  ,如果后台是放在/project文件中时要更改config  index里面buildasset是publicpath路径为/project，如果在加载页面app.js文件过大 建议用异步加载组件

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '@/page/home/Home'
-import City from '@/page/city/City'
-import Detail from '@/page/detail/Detail'
+//import Home from '@/page/home/Home'
+//import City from '@/page/city/City'
+//import Detail from '@/page/detail/Detail'
 
-
+// 异步加载组件（）=>import("")
 Vue.use(Router)
 
 export default new Router({
@@ -12,16 +12,16 @@ export default new Router({
     {
       path: '/',
       name: 'Home',
-      component: Home
+      component: () =>import('@/page/home/Home')
     },
     {
       path:'/city',
       name:'City',
-      component:City
+      component:() =>import('@/page/city/City')
     },{
       path:'/detail/:id',
       name:'Detail',
-      component:Detail
+      component:() =>import('@/page/detail/Detail')
     }
   ]
 })
